0543a16268a53686f0e0fe04b71f583d788f5de5
Rockallite Wulf <rockallite.wulf@gmail.com>
2017-05-11 00:20:03 +0800

    Update Django checking

--- django_smartstaticfiles/__init__.py
+++ django_smartstaticfiles/__init__.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-from django import VERSION
+from django import VERSION as DJANGO_VERSION
 
-PROPER_VERSION = (1, 11)
-assert VERSION[0] == PROPER_VERSION[0] and VERSION[1] == PROPER_VERSION[1], \
-    '%s is only compatible with Django %s.%s' % ((__name__,) + PROPER_VERSION)
+PROPER_DJANGO_VERSION = (1, 11)
+
+assert DJANGO_VERSION[0] == PROPER_DJANGO_VERSION[0] and \
+    DJANGO_VERSION[1] == PROPER_DJANGO_VERSION[1], \
+    '%s is only compatible with Django %s.%s' % (
+        (__name__,) + PROPER_DJANGO_VERSION
+    )
